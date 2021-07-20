b5d0f61a307322a93df996d8d98ed8ba0817546e
Ayush <ayushmodi2003@gmail.com>
2021-07-20 12:05:30 +0530

    Change config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,16 @@
 // Imports
+require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
+const dotenv = require("dotenv");
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+dotenv.config();
+
 module.exports = {
   solidity: "0.8.0",
   networks: {
+    rinkeby: {
+        url: process.env.RINKEBY_JSONRPC_HTTP_URL,
+        accounts: [process.env.WALLET_PRIVATE_KEY_1],
+      }
   },
 };
