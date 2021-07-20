28178761ce5ad67b7eb07d3426c67d7f39907178
Ayush <ayushmodi2003@gmail.com>
2021-07-20 07:41:10 +0530

    Add hardhat config

---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+// Imports
+require("@nomiclabs/hardhat-waffle");
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+  },
+};
